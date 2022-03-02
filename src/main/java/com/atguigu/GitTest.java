ee833e874318962e620cca79ad0f1d79a32644b8
HanQi <123456@qq.com>
2022-03-02 20:11:59 +0800

    second commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -3,5 +3,7 @@ package com.atguigu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!2");
+
     }
 }
